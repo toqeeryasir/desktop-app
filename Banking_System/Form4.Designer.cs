0e89a2c5dc112d96f5971b4ef28dd53fe30c6e0a
Toqeer Yasir <miantoqeeryasir0@gmail.com>
2025-01-07 19:53:19 +0500

    added form4 designer

---
+++ Banking_System/Form4.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Banking_System
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form4));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.add_balance_textbox = new System.Windows.Forms.TextBox();
+            this.add_balance_add_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Georgia", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Snow;
+            this.label1.Location = new System.Drawing.Point(139, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Add Balance";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Georgia", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label2.ForeColor = System.Drawing.Color.Snow;
+            this.label2.Location = new System.Drawing.Point(33, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Add Balance";
+            // 
+            // add_balance_textbox
+            // 
+            this.add_balance_textbox.Location = new System.Drawing.Point(190, 101);
+            this.add_balance_textbox.Name = "add_balance_textbox";
+            this.add_balance_textbox.Size = new System.Drawing.Size(187, 20);
+            this.add_balance_textbox.TabIndex = 2;
+            // 
+            // add_balance_add_button
+            // 
+            this.add_balance_add_button.BackColor = System.Drawing.Color.Transparent;
+            this.add_balance_add_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.add_balance_add_button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.add_balance_add_button.Font = new System.Drawing.Font("Georgia", 13F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))));
+            this.add_balance_add_button.ForeColor = System.Drawing.Color.Snow;
+            this.add_balance_add_button.Location = new System.Drawing.Point(68, 166);
+            this.add_balance_add_button.Name = "add_balance_add_button";
+            this.add_balance_add_button.Size = new System.Drawing.Size(281, 41);
+            this.add_balance_add_button.TabIndex = 3;
+            this.add_balance_add_button.Text = "Add";
+            this.add_balance_add_button.UseVisualStyleBackColor = false;
+            this.add_balance_add_button.Click += new System.EventHandler(this.add_balance_add_button_Click);
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(417, 253);
+            this.Controls.Add(this.add_balance_add_button);
+            this.Controls.Add(this.add_balance_textbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form4";
+            this.Text = "Form4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox add_balance_textbox;
+        private System.Windows.Forms.Button add_balance_add_button;
+    }
+}
