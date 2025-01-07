d71b55df21e3709b49eeb932ab84920bd72b7beb
Toqeer Yasir <miantoqeeryasir0@gmail.com>
2025-01-07 19:51:49 +0500

    added form3 designer

---
+++ Banking_System/Form3.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace Banking_System
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.full_name_textbox = new System.Windows.Forms.TextBox();
+            this.dob_textbox = new System.Windows.Forms.TextBox();
+            this.address_textbox = new System.Windows.Forms.TextBox();
+            this.contact_textbox = new System.Windows.Forms.TextBox();
+            this.email_textbox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.username_textbox = new System.Windows.Forms.TextBox();
+            this.password_textbox = new System.Windows.Forms.TextBox();
+            this.cnic_textbox = new System.Windows.Forms.TextBox();
+            this.account_no_textbox = new System.Windows.Forms.TextBox();
+            this.initial_deposit_textbox = new System.Windows.Forms.TextBox();
+            this.sign_up_next_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Georgia", 16F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label1.ForeColor = System.Drawing.Color.Snow;
+            this.label1.Location = new System.Drawing.Point(364, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sign_up";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Georgia", 10F, System.Drawing.FontStyle.Italic);
+            this.label2.ForeColor = System.Drawing.Color.Snow;
+            this.label2.Location = new System.Drawing.Point(45, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Full Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Georgia", 10F, System.Drawing.FontStyle.Italic);
+            this.label3.ForeColor = System.Drawing.Color.Snow;
+            this.label3.Location = new System.Drawing.Point(45, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Date of Birth";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Georgia", 10F, System.Drawing.FontStyle.Italic);
+            this.label4.ForeColor = System.Drawing.Color.Snow;
+            this.label4.Location = new System.Drawing.Point(45, 207);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Address";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Georgia", 10F, System.Drawing.FontStyle.Italic);
+            this.label5.ForeColor = System.Drawing.Color.Snow;
+            this.label5.Location = new System.Drawing.Point(45, 256);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(113, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Contact Number";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Georgia", 10F, System.Drawing.FontStyle.Italic);
+            this.label6.ForeColor = System.Drawing.Color.Snow;
+            this.label6.Location = new System.Drawing.Point(45, 304);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Email Address";
+            // 
+            // full_name_textbox
+            // 
+            this.full_name_textbox.Location = new System.Drawing.Point(193, 113);
+            this.full_name_textbox.Name = "full_name_textbox";
+            this.full_name_textbox.Size = new System.Drawing.Size(187, 20);
+            this.full_name_textbox.TabIndex = 6;
+            // 
+            // dob_textbox
+            // 
+            this.dob_textbox.Location = new System.Drawing.Point(193, 159);
+            this.dob_textbox.Name = "dob_textbox";
+            this.dob_textbox.Size = new System.Drawing.Size(187, 20);
+            this.dob_textbox.TabIndex = 7;
+            // 
+            // address_textbox
+            // 
+            this.address_textbox.Location = new System.Drawing.Point(193, 206);
+            this.address_textbox.Name = "address_textbox";
+            this.address_textbox.Size = new System.Drawing.Size(187, 20);
+            this.address_textbox.TabIndex = 8;
+            // 
+            // contact_textbox
+            // 
+            this.contact_textbox.Location = new System.Drawing.Point(193, 255);
+            this.contact_textbox.Name = "contact_textbox";
+            this.contact_textbox.Size = new System.Drawing.Size(187, 20);
+            this.contact_textbox.TabIndex = 9;
+            // 
+            // email_textbox
+            // 
+            this.email_textbox.Location = new System.Drawing.Point(193, 303);
+            this.email_textbox.Name = "email_textbox";
+            this.email_textbox.Size = new System.Drawing.Size(187, 20);
+            this.email_textbox.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Georgia", 10F, System.Drawing.FontStyle.Italic);
+            this.label7.ForeColor = System.Drawing.Color.Snow;
+            this.label7.Location = new System.Drawing.Point(438, 113);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(73, 17);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Username";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Georgia", 10F, System.Drawing.FontStyle.Italic);
+            this.label8.ForeColor = System.Drawing.Color.Snow;
+            this.label8.Location = new System.Drawing.Point(438, 160);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(71, 17);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Password";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Georgia", 10F, System.Drawing.FontStyle.Italic);
+            this.label9.ForeColor = System.Drawing.Color.Snow;
+            this.label9.Location = new System.Drawing.Point(438, 206);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(68, 17);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "CNIC No.";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Font = new System.Drawing.Font("Georgia", 10F, System.Drawing.FontStyle.Italic);
+            this.label10.ForeColor = System.Drawing.Color.Snow;
+            this.label10.Location = new System.Drawing.Point(438, 255);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(86, 17);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "Account No.";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.Color.Transparent;
+            this.label11.Font = new System.Drawing.Font("Georgia", 10F, System.Drawing.FontStyle.Italic);
+            this.label11.ForeColor = System.Drawing.Color.Snow;
+            this.label11.Location = new System.Drawing.Point(438, 303);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(97, 17);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "Initial Deposit";
+            // 
+            // username_textbox
+            // 
+            this.username_textbox.Location = new System.Drawing.Point(570, 113);
+            this.username_textbox.Name = "username_textbox";
+            this.username_textbox.Size = new System.Drawing.Size(187, 20);
+            this.username_textbox.TabIndex = 16;
+            // 
+            // password_textbox
+            // 
+            this.password_textbox.Location = new System.Drawing.Point(570, 160);
+            this.password_textbox.Name = "password_textbox";
+            this.password_textbox.Size = new System.Drawing.Size(187, 20);
+            this.password_textbox.TabIndex = 17;
+            // 
+            // cnic_textbox
+            // 
+            this.cnic_textbox.Location = new System.Drawing.Point(570, 207);
+            this.cnic_textbox.Name = "cnic_textbox";
+            this.cnic_textbox.Size = new System.Drawing.Size(187, 20);
+            this.cnic_textbox.TabIndex = 18;
+            // 
+            // account_no_textbox
+            // 
+            this.account_no_textbox.Location = new System.Drawing.Point(570, 256);
+            this.account_no_textbox.Name = "account_no_textbox";
+            this.account_no_textbox.Size = new System.Drawing.Size(187, 20);
+            this.account_no_textbox.TabIndex = 19;
+            // 
+            // initial_deposit_textbox
+            // 
+            this.initial_deposit_textbox.Location = new System.Drawing.Point(570, 304);
+            this.initial_deposit_textbox.Name = "initial_deposit_textbox";
+            this.initial_deposit_textbox.Size = new System.Drawing.Size(187, 20);
+            this.initial_deposit_textbox.TabIndex = 20;
+            // 
+            // sign_up_next_button
+            // 
+            this.sign_up_next_button.BackColor = System.Drawing.Color.Transparent;
+            this.sign_up_next_button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.sign_up_next_button.Font = new System.Drawing.Font("Georgia", 13F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))));
+            this.sign_up_next_button.ForeColor = System.Drawing.Color.Snow;
+            this.sign_up_next_button.Location = new System.Drawing.Point(268, 361);
+            this.sign_up_next_button.Name = "sign_up_next_button";
+            this.sign_up_next_button.Size = new System.Drawing.Size(281, 41);
+            this.sign_up_next_button.TabIndex = 21;
+            this.sign_up_next_button.Text = "Next";
+            this.sign_up_next_button.UseVisualStyleBackColor = false;
+            this.sign_up_next_button.Click += new System.EventHandler(this.sign_up_next_button_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.sign_up_next_button);
+            this.Controls.Add(this.initial_deposit_textbox);
+            this.Controls.Add(this.account_no_textbox);
+            this.Controls.Add(this.cnic_textbox);
+            this.Controls.Add(this.password_textbox);
+            this.Controls.Add(this.username_textbox);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.email_textbox);
+            this.Controls.Add(this.contact_textbox);
+            this.Controls.Add(this.address_textbox);
+            this.Controls.Add(this.dob_textbox);
+            this.Controls.Add(this.full_name_textbox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox full_name_textbox;
+        private System.Windows.Forms.TextBox dob_textbox;
+        private System.Windows.Forms.TextBox address_textbox;
+        private System.Windows.Forms.TextBox contact_textbox;
+        private System.Windows.Forms.TextBox email_textbox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox username_textbox;
+        private System.Windows.Forms.TextBox password_textbox;
+        private System.Windows.Forms.TextBox cnic_textbox;
+        private System.Windows.Forms.TextBox account_no_textbox;
+        private System.Windows.Forms.TextBox initial_deposit_textbox;
+        private System.Windows.Forms.Button sign_up_next_button;
+    }
+}
