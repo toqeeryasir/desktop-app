c686f0b98a90ae6017617e059e25ff6b318368cb
Toqeer Yasir <miantoqeeryasir0@gmail.com>
2025-01-08 19:38:57 +0500

    added form5 designer

---
+++ Banking_System/Form5.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Banking_System
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form5));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.check_balance_textbox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.check_balance_refresh_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Georgia", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label1.Location = new System.Drawing.Point(135, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Check Balance";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Georgia", 10F, System.Drawing.FontStyle.Italic);
+            this.label2.Location = new System.Drawing.Point(38, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Current Balance";
+            // 
+            // check_balance_textbox
+            // 
+            this.check_balance_textbox.Location = new System.Drawing.Point(184, 92);
+            this.check_balance_textbox.Name = "check_balance_textbox";
+            this.check_balance_textbox.Size = new System.Drawing.Size(194, 20);
+            this.check_balance_textbox.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Georgia", 7F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(38, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(191, 26);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "!! Please make sure the balance shown\r\n   is correct .";
+            // 
+            // check_balance_refresh_button
+            // 
+            this.check_balance_refresh_button.BackColor = System.Drawing.Color.Transparent;
+            this.check_balance_refresh_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.check_balance_refresh_button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.check_balance_refresh_button.Font = new System.Drawing.Font("Georgia", 13F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))));
+            this.check_balance_refresh_button.Location = new System.Drawing.Point(70, 136);
+            this.check_balance_refresh_button.Name = "check_balance_refresh_button";
+            this.check_balance_refresh_button.Size = new System.Drawing.Size(281, 41);
+            this.check_balance_refresh_button.TabIndex = 4;
+            this.check_balance_refresh_button.Text = "Refresh";
+            this.check_balance_refresh_button.UseVisualStyleBackColor = false;
+            this.check_balance_refresh_button.Click += new System.EventHandler(this.check_balance_done_button_Click);
+            // 
+            // Form5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(417, 253);
+            this.Controls.Add(this.check_balance_refresh_button);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.check_balance_textbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Snow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form5";
+            this.Text = "Form5";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox check_balance_textbox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button check_balance_refresh_button;
+    }
+}
