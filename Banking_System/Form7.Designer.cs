b6a21f1c266a888db051e68d6e66d41353c9a838
Toqeer Yasir <miantoqeeryasir0@gmail.com>
2025-01-09 22:16:34 +0500

    added form7 designer

---
+++ Banking_System/Form7.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Banking_System
+{
+    partial class Form7
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form7));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.edit_full_name_textbox = new System.Windows.Forms.TextBox();
+            this.edit_contact_no_textbox = new System.Windows.Forms.TextBox();
+            this.edit_email_textbox = new System.Windows.Forms.TextBox();
+            this.edit_address_textbox = new System.Windows.Forms.TextBox();
+            this.edit_account_button = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Georgia", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Snow;
+            this.label1.Location = new System.Drawing.Point(236, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Edit Account info.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Georgia", 10F, System.Drawing.FontStyle.Italic);
+            this.label2.ForeColor = System.Drawing.Color.Snow;
+            this.label2.Location = new System.Drawing.Point(46, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Full Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Georgia", 10F, System.Drawing.FontStyle.Italic);
+            this.label3.ForeColor = System.Drawing.Color.Snow;
+            this.label3.Location = new System.Drawing.Point(46, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Contact No.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Georgia", 10F, System.Drawing.FontStyle.Italic);
+            this.label4.ForeColor = System.Drawing.Color.Snow;
+            this.label4.Location = new System.Drawing.Point(345, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Email";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Georgia", 10F, System.Drawing.FontStyle.Italic);
+            this.label5.ForeColor = System.Drawing.Color.Snow;
+            this.label5.Location = new System.Drawing.Point(345, 169);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Address";
+            // 
+            // edit_full_name_textbox
+            // 
+            this.edit_full_name_textbox.Location = new System.Drawing.Point(162, 114);
+            this.edit_full_name_textbox.Name = "edit_full_name_textbox";
+            this.edit_full_name_textbox.Size = new System.Drawing.Size(149, 20);
+            this.edit_full_name_textbox.TabIndex = 5;
+            // 
+            // edit_contact_no_textbox
+            // 
+            this.edit_contact_no_textbox.Location = new System.Drawing.Point(162, 168);
+            this.edit_contact_no_textbox.Name = "edit_contact_no_textbox";
+            this.edit_contact_no_textbox.Size = new System.Drawing.Size(149, 20);
+            this.edit_contact_no_textbox.TabIndex = 6;
+            // 
+            // edit_email_textbox
+            // 
+            this.edit_email_textbox.Location = new System.Drawing.Point(435, 114);
+            this.edit_email_textbox.Name = "edit_email_textbox";
+            this.edit_email_textbox.Size = new System.Drawing.Size(149, 20);
+            this.edit_email_textbox.TabIndex = 7;
+            // 
+            // edit_address_textbox
+            // 
+            this.edit_address_textbox.Location = new System.Drawing.Point(435, 168);
+            this.edit_address_textbox.Name = "edit_address_textbox";
+            this.edit_address_textbox.Size = new System.Drawing.Size(149, 20);
+            this.edit_address_textbox.TabIndex = 8;
+            // 
+            // edit_account_button
+            // 
+            this.edit_account_button.BackColor = System.Drawing.Color.Transparent;
+            this.edit_account_button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.edit_account_button.Font = new System.Drawing.Font("Georgia", 13F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))));
+            this.edit_account_button.ForeColor = System.Drawing.Color.Snow;
+            this.edit_account_button.Location = new System.Drawing.Point(194, 217);
+            this.edit_account_button.Name = "edit_account_button";
+            this.edit_account_button.Size = new System.Drawing.Size(281, 41);
+            this.edit_account_button.TabIndex = 9;
+            this.edit_account_button.Text = "Update";
+            this.edit_account_button.UseVisualStyleBackColor = false;
+            this.edit_account_button.Click += new System.EventHandler(this.edit_account_button_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Georgia", 8F, System.Drawing.FontStyle.Italic);
+            this.label6.ForeColor = System.Drawing.Color.Snow;
+            this.label6.Location = new System.Drawing.Point(46, 282);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(170, 28);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "!! You can only edit the above\r\n    specified attributes .";
+            // 
+            // Form7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(646, 330);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.edit_account_button);
+            this.Controls.Add(this.edit_address_textbox);
+            this.Controls.Add(this.edit_email_textbox);
+            this.Controls.Add(this.edit_contact_no_textbox);
+            this.Controls.Add(this.edit_full_name_textbox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form7";
+            this.Text = "Form7";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox edit_full_name_textbox;
+        private System.Windows.Forms.TextBox edit_contact_no_textbox;
+        private System.Windows.Forms.TextBox edit_email_textbox;
+        private System.Windows.Forms.TextBox edit_address_textbox;
+        private System.Windows.Forms.Button edit_account_button;
+        private System.Windows.Forms.Label label6;
+    }
+}
